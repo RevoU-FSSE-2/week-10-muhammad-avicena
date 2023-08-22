14c250a682ea8fe42ef78629ed244f2b4c254b6c
Hideaki-Ryu <cenarahmant@gmail.com>
2023-08-22 16:22:09 +0700

    feat: add connectMongoDB function and router

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ const http = require("http");
 const createError = require("http-errors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const connectMongoDb = require("./src/middleware/dbConfig");
 require("dotenv").config();
 
 const app = express();
 
-// Import routes
+// Import router
 const indexRoutes = require("./src/routes/indexRoutes");
+const userRoutes = require("./src/routes/userRoutes");
 
 // Middleware
 app.use(logger("dev"));
 app.use(bodyParser.json());
 
-// Router
+// App Router
 app.use("/", indexRoutes);
+app.use("/api/v1/users", userRoutes);
 
 // App listeners
 const server = http.createServer(app);
@@ -23,6 +26,17 @@ const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log("Running on http://localhost:" + PORT);
 });
+
+// MongoDB connection
+async function connectDatabase() {
+  try {
+    await connectMongoDb();
+    console.log("Succesfully connected to MongoDB");
+  } catch (error) {
+    console.error("Error connected to database:", error);
+  }
+}
+connectDatabase();
 
 // Error handlers
 app.use(function (req, res, next) {
