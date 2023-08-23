ca87fcd8601c9c839f3e13c88824cb078c33f353
Hideaki-Ryu <cenarahmant@gmail.com>
2023-08-24 04:19:36 +0700

    chore: loginUser, registerUser, & getAllUser

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 const AuthDao = require("../dao/authDao");
-const connectMongoDB = require("../middleware/dbConfig");
+const connectMongoDB = require("../middleware/config/dbConfig");
 const AuthService = require("../services/authService");
 
 async function loginUser(req, res) {
@@ -12,7 +12,7 @@ async function loginUser(req, res) {
     const result = await authService.loginUser(username, password);
 
     if (result.success) {
-      res.status(200).json({
+      return res.status(200).json({
         success: true,
         message: "Successfully logged in",
         data: { token: result.token },
@@ -22,34 +22,34 @@ async function loginUser(req, res) {
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ success: false, message: error.message });
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 }
 
 async function registerUser(req, res) {
-    const { username, password, role } = req.body;
-  
-    if (username && password && role) {
-      const db = await connectMongoDB(); 
-      const userDao = new AuthDao(db);
-      const userService = new AuthService(userDao);
-      const result = await userService.registerUser(username, password, role);
-  
-      if (result.success) {
-        res.status(200).json({
-          success: true,
-          message: "Successfully created a user",
-          data: { _id: result._id },
-        });
-      } else {
-        res.status(400).json(result);
-      }
+  const { username, password, role } = req.body;
+
+  if (username && password && role) {
+    const db = await connectMongoDB();
+    const userDao = new AuthDao(db);
+    const userService = new AuthService(userDao);
+    const result = await userService.registerUser(username, password, role);
+
+    if (result.success) {
+      return res.status(200).json({
+        success: true,
+        message: "Successfully created a user",
+        data: { _id: result._id },
+      });
     } else {
-      res.status(400).json({ success: false, message: "Invalid input data" });
+      return res.status(400).json(result);
     }
+  } else {
+    return res.status(400).json({ success: false, message: "Invalid input data" });
   }
+}
 
 module.exports = {
   loginUser,
-  registerUser
+  registerUser,
 };
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,30 +1,31 @@
 const UserDao = require("../dao/userDao");
-const connectMongoDB = require("../middleware/dbConfig");
-const UserService = require('../services/userService'); 
+const connectMongoDB = require("../middleware/config/dbConfig");
+const UserService = require("../services/userService");
 
 async function getAllUsers(req, res) {
   try {
-    const db = await connectMongoDB(); 
+    const db = await connectMongoDB();
     const userDao = new UserDao(db);
     const userService = new UserService(userDao);
     const result = await userService.getAllUsers();
 
     if (result.success) {
-      res.status(200).json({
+      return res.status(200).json({
         success: true,
         message: "List of users",
         data: result.users,
       });
     } else {
-      res.status(500).json({ success: false, message: result.message });
+      return res.status(500).json({ success: false, message: result.message });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
   }
 }
 
-
 module.exports = {
-  getAllUsers
+  getAllUsers,
 };
