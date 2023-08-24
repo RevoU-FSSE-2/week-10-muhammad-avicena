4e5cb59606f7306483982febbbbcea5e3a994756
Hideaki-Ryu <cenarahmant@gmail.com>
2023-08-25 04:46:53 +0700

    feat: add router transfer, swaggerUi api documentation, auth middleware

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const http = require("http");
 const createError = require("http-errors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-const connectMongoDb = require("./src/middleware/dbConfig");
+const swaggerUi = require("swagger-ui-express");
+const connectMongoDb = require("./src/middleware/config/dbConfig");
+const {
+  userAuthentication,
+  adminAuthorization,
+} = require("./src/middleware/authMiddleware");
 require("dotenv").config();
+const yaml = require("yaml");
+const fs = require("fs");
 
 const app = express();
 
@@ -12,15 +19,23 @@ const app = express();
 const indexRoutes = require("./src/routes/indexRoutes");
 const authRoutes = require("./src/routes/authRoutes");
 const userRoutes = require("./src/routes/userRoutes");
+const transferRoutes = require("./src/routes/transferRoutes");
 
 // Middleware
 app.use(logger("dev"));
 app.use(bodyParser.json());
 
+// APi Documentation
+const apiDocs = "api-docs.yaml";
+const readApiFile = fs.readFileSync(apiDocs, "utf8");
+const swaggerDocs = yaml.parse(readApiFile);
+
 // App Router
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use("/", indexRoutes);
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/users", adminAuthorization, userRoutes);
+app.use("/api/v1/transfers", userAuthentication, transferRoutes);
 
 // App listeners
 const server = http.createServer(app);
