6b0164edd8edfaa373927f540ec3534f31b191f1
Hideaki-Ryu <cenarahmant@gmail.com>
2023-08-25 19:45:57 +0700

    chore: redirect indexRoutes to api-documentation

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World !" });
+  res.status(200).redirect("/api-docs");
 });
 
 module.exports = router;
